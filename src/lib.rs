762f619cbde99d77bb0982f1091ab7d45d34a67b
tamaina <tamaina@hotmail.co.jp>
2025-07-29 02:33:40 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,102 @@
 mod utils;
 
+use std::io::Cursor;
+use serde::{Deserialize, Serialize};
+use tsify::Tsify;
 use wasm_bindgen::prelude::*;
+use web_sys::{File, Blob, BlobPropertyBag};
+use js_sys::{ArrayBuffer, Uint8Array};
+use wasm_bindgen_futures::JsFuture;
+use image::{ImageReader};
+use image::imageops::FilterType;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[derive(Debug, Tsify, Serialize, Deserialize)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub enum ImageRsImageResizeAlgorithm {
+    #[serde(rename = "nearest")]
+    Nearest,
+    #[serde(rename = "triangle")]
+    Triangle,
+    #[serde(rename = "catmull-rom")]
+    CatmullRom,
+    #[serde(rename = "gaussian")]
+    Gaussian,
+    #[serde(rename = "lanczos3")]
+    Lanczos3,
+}
+
+impl From<ImageRsImageResizeAlgorithm> for FilterType {
+    fn from(algorithm: ImageRsImageResizeAlgorithm) -> Self {
+        match algorithm {
+            ImageRsImageResizeAlgorithm::Nearest => FilterType::Nearest,
+            ImageRsImageResizeAlgorithm::Triangle => FilterType::Triangle,
+            ImageRsImageResizeAlgorithm::CatmullRom => FilterType::CatmullRom,
+            ImageRsImageResizeAlgorithm::Gaussian => FilterType::Gaussian,
+            ImageRsImageResizeAlgorithm::Lanczos3 => FilterType::Lanczos3,
+        }
+    }
+}
+
+/// Configuration for browser image resizing
+#[derive(Debug, Tsify, Serialize, Deserialize)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct BrowserImageResizerConfig {
+    pub algorithm: ImageRsImageResizeAlgorithm,
+
+    pub max_width: u32,
+    pub max_height: u32,
+
+    #[tsify(optional)]
+    pub scale_ratio: Option<f32>,
+
+    #[tsify(optional)]
+    pub debug: Option<bool>,
+
+    #[tsify(optional)]
+    pub quality: Option<f32>,
+
+    #[tsify(optional)]
+    pub mime_type: String,
+}
+
+async fn file_to_vec(i: &File) -> Result<Vec<u8>, JsValue> {
+    JsFuture::from(i.array_buffer()).await
+        .map(|js_val| js_val.dyn_into::<ArrayBuffer>().unwrap())
+        .map(|array_buffer| Uint8Array::new(&array_buffer).to_vec())
+}
+
+#[wasm_bindgen]  
+pub fn vec_to_blob(data: Vec<u8>, mime_type: &str) -> Result<Blob, JsValue> {
+    let uint8_array = Uint8Array::from(&data[..]);  
+
+    let blob_options = BlobPropertyBag::new();  
+    blob_options.set_type(mime_type); // MIMEタイプを設定
+
+    Blob::new_with_u8_array_sequence_and_options(&uint8_array, &blob_options)
 }
 
 #[wasm_bindgen]
-pub fn greet(s: &str) {
-    alert(&format!("Hello, {}!", s));
+pub async fn read_and_compress_image(i: &File, config: BrowserImageResizerConfig) -> Vec<u8> {
+    // Placeholder for image processing logic
+    // This function should read the image file, compress it, and return a new File object
+    // For now, we will just return the input file as is
+    let u8vec = file_to_vec(&i).await.unwrap();
+    let buffered_reader = Cursor::new(u8vec);
+    let image_reader: ImageReader<Cursor<Vec<u8>>> = ImageReader::new(buffered_reader).with_guessed_format().unwrap();
+    let image = image_reader.decode().unwrap();
+    log(&format!("Image dimensions: {}x{}", image.width(), image.height()));
+    log(&format!("image color: {:?}", image.color()));
+    log(&format!("{:?}", config));
+    let image2 = image.resize(config.max_width, config.max_height, config.algorithm.into());
+    let mut writer = Vec::new();
+    image2.write_to(&mut Cursor::new(&mut writer), image::ImageFormat::Png)
+        .expect("Failed to write image");
+    log("Image compression completed");
+    writer
 }
--- test/src/components/HelloWorld.vue
+++ test/src/components/HelloWorld.vue
@@ -1,10 +1,38 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import init, { greet } from 'image-rs-wasm'
+import init, { read_and_compress_image } from 'image-rs-wasm'
+import type { BrowserImageResizerConfig } from 'image-rs-wasm'
 
 defineProps<{ msg: string }>()
 
 const wasmLoaded = ref(false)
+const file = ref<File | null>(null)
+const url = ref<string | null>(null)
+
+function onFileChange(event: Event) {
+  const target = event.target as HTMLInputElement
+  if (target.files && target.files.length > 0) {
+    file.value = target.files[0]
+    console.log('Selected file:', file.value)
+  }
+}
+
+async function onClick() {
+  if (file.value) {
+    const config: BrowserImageResizerConfig = {
+      algorithm: 'catmull-rom',
+      max_width: 1024,
+      max_height: 1024,
+      quality: 0.8,
+      mime_type: 'image/jpeg',
+    }
+    const u8 = await read_and_compress_image(file.value, config) as Uint8Array<ArrayBuffer>
+    console.log('Compressed image blob:', u8)
+    url.value = URL.createObjectURL(new Blob([u8], { type: config.mime_type }))
+  } else {
+    console.warn('No file selected')
+  }
+}
 
 init().then(() => {
   wasmLoaded.value = true
@@ -14,29 +42,14 @@ init().then(() => {
 <template>
   <h1>{{ msg }}</h1>
 
+  <input type="file" accept="image/*" @change="onFileChange" />
   <div class="card">
-    <button v-if="wasmLoaded" type="button" @click="greet(msg)">GREET</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button v-if="wasmLoaded" type="button" @click="onClick">JUST DO IT</button>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div v-if="url" class="card">
+    <img :src="url" alt="Compressed Image" />
+    <p><a :href="url" download="compressed_image.png">Download Compressed Image</a></p>
+  </div>
 </template>
 
 <style scoped>
